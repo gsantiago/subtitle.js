ff0f1eae3b7154ea8dff29ecbd7682bcdf8915ff
Guilherme Santiago <guilherme.santiago95@gmail.com>
2015-04-26 20:15:23 -0300

    Add two static methods for time conversion

--- subtitle.js
+++ subtitle.js
@@ -75,4 +75,72 @@ Subtitle.prototype.parse = function () {
   return subs;
 };
 
+
+/**
+ * Convert the SRT time format to milliseconds
+ *
+ * @static
+ * @param {String} SRT time format
+*/
+Subtitle.toMS = function (time) {
+  var match = time.match(/^(\d{2}):(\d{2}):(\d{2}),(\d{3})$/);
+
+  if (!match) {
+    throw new Error('Invalid SRT time format');
+  }
+
+  var hours = parseInt(match[1], 10);
+  var minutes = parseInt(match[2], 10);
+  var seconds = parseInt(match[3], 10);
+  var milliseconds = parseInt(match[4], 10);
+
+  hours *= 3600000;
+  minutes *= 60000;
+  seconds *= 1000;
+
+  return hours + minutes + seconds + milliseconds;
+};
+
+
+/**
+ * Convert milliseconds to SRT time format
+ *
+ * @static
+ * @param {Integer} Milleseconds
+*/
+Subtitle.toSrtTime = function (time) {
+  if (!/^\d+$/.test(time.toString())) {
+    throw new Error('Time should be an Integer value in milliseconds');
+  }
+
+  time = parseInt(time);
+
+  var date = new Date(0, 0, 0, 0, 0, 0, time);
+
+  var hours = date.getHours() < 10
+    ? '0' + date.getHours()
+    : date.getHours();
+
+  var minutes = date.getMinutes() < 10
+    ? '0' + date.getMinutes()
+    : date.getMinutes();
+
+  var seconds = date.getSeconds() < 10
+    ? '0' + date.getSeconds()
+    : date.getSeconds();
+
+  var ms = time - ((hours * 3600000) + (minutes * 60000) + (seconds * 1000));
+
+  if (ms < 100 && ms > 10) {
+    ms = '0' + ms;
+  } else if (ms < 10) {
+    ms = '00' + ms;
+  }
+
+  var srtTime = hours + ':' + minutes + ':' + seconds + ',' + ms;
+
+  return srtTime;
+};
+
+
 module.exports = Subtitle;

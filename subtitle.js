0f90296f761e1fb7a3478ffbb3770ea74bb0a57b
Guilherme Santiago <guilherme.santiago95@gmail.com>
2015-05-02 17:57:44 -0300

    Add strinfy method

--- subtitle.js
+++ subtitle.js
@@ -242,4 +242,27 @@ Subtitle.prototype.getSubtitles = function (options) {
 };
 
 
+/**
+ * Returns the subtitles in SRT string
+ * @returns {String} srt
+*/
+Subtitle.prototype.stringfy = function () {
+  var self = this;
+  var buffer = '';
+
+  this._subtitles.forEach(function (caption, index) {
+    if (index > 0) {
+      buffer += '\n';
+    }
+    buffer += caption.index;
+    buffer += '\n';
+    buffer += caption.start + ' --> ' + caption.end;
+    buffer += '\n';
+    buffer += caption.text;
+    buffer += '\n';
+  });
+
+  return buffer;
+};
+
 module.exports = Subtitle;

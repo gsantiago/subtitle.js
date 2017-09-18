80c7a94cfe41bb7db600d473267891d89189eda5
Guilherme Santiago <guilherme.santiago95@gmail.com>
2017-09-18 09:14:40 -0300

    Add build

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+exports.toMS = require('./lib/toMS')
+exports.toSrtTime = require('./lib/toSrtTime')
+exports.parse = require('./lib/parse')
+exports.stringify = require('./lib/stringify')
+exports.resync = require('./lib/resync')

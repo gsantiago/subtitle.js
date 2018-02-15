2e82b8b23e06ff3de6af8ad43f8ad8665750f0cb
Marnus Weststrate <marnusw@gmail.com>
2018-02-15 11:08:19 +0000

    Support for both SRT and WebVTT input strings (#21)

--- test/toMS.test.js
+++ test/toMS.test.js
@@ -1,7 +1,7 @@
 const test = require('ava')
-const { toMS } = require('..')
+const {toMS} = require('..')
 
-test('should convert time to milliseconds', t => {
+test('should convert SRT time to milliseconds', t => {
   const time1 = {
     srt: '00:02:22,542',
     ms: 120000 + 22000 + 542
@@ -20,6 +20,33 @@ test('should convert time to milliseconds', t => {
   t.is(toMS(time1.srt), time1.ms)
   t.is(toMS(time2.srt), time2.ms)
   t.is(toMS(time3.srt), time3.ms)
+})
+
+test('should convert VTT time to milliseconds including short formats', t => {
+  const time1 = {
+    srt: '02:22.542',
+    ms: 120000 + 22000 + 542
+  }
+
+  const time2 = {
+    srt: '01:51:58.219',
+    ms: 3600000 + 3060000 + 58000 + 219
+  }
+
+  const time3 = {
+    srt: '00:00.000',
+    ms: 0
+  }
+
+  const time4 = {
+    srt: '1201:51:58.219',
+    ms: (1201 * 3600000) + 3060000 + 58000 + 219
+  }
+
+  t.is(toMS(time1.srt), time1.ms)
+  t.is(toMS(time2.srt), time2.ms)
+  t.is(toMS(time3.srt), time3.ms)
+  t.is(toMS(time4.srt), time4.ms)
 })
 
 test('invalid format should throw an error', t => {

cab3f6afc5d52d167c0479355e310949342ee5ca
Guilherme Santiago <guilherme.santiago95@gmail.com>
2020-08-28 16:23:27 -0300

    refactor tests with test.each

--- test/parseTimestamps.test.js
+++ test/parseTimestamps.test.js
@@ -1,83 +1,110 @@
 import { parseTimestamps } from '../lib/parseTimestamps'
 
-const check = (val, expected) => expect(parseTimestamps(val)).toEqual(expected)
-
-test('parseTimestamps with SRT input', () => {
-  check('00:00:20,000 --> 00:00:24,400', {
-    start: 20000,
-    end: 24400
-  })
-
-  check('00:00:00,000 --> 00:00:01,000', {
-    start: 0,
-    end: 1000
-  })
-
-  check('00:01:00,500 --> 00:01:10,800', {
-    start: 60500,
-    end: 70800
-  })
-
-  check('12:34:56,789 --> 98:76:54,321', {
-    start: 45296789,
-    end: 357414321
-  })
-
-  check('0:59:50,050 --> 1:00:20,070', {
-    start: 3590050,
-    end: 3620070
-  })
-
-  check('0:00:20,000 --> 0:00:24,400', {
-    start: 20000,
-    end: 24400
-  })
+test.each([
+  [
+    '00:00:20,000 --> 00:00:24,400',
+    {
+      start: 20000,
+      end: 24400
+    }
+  ],
+  [
+    '00:00:00,000 --> 00:00:01,000',
+    {
+      start: 0,
+      end: 1000
+    }
+  ],
+  [
+    '00:01:00,500 --> 00:01:10,800',
+    {
+      start: 60500,
+      end: 70800
+    }
+  ],
+  [
+    '12:34:56,789 --> 98:76:54,321',
+    {
+      start: 45296789,
+      end: 357414321
+    }
+  ],
+  [
+    '0:59:50,050 --> 1:00:20,070',
+    {
+      start: 3590050,
+      end: 3620070
+    }
+  ],
+  [
+    '0:00:20,000 --> 0:00:24,400',
+    {
+      start: 20000,
+      end: 24400
+    }
+  ]
+])('parseTimestamps with SRT input: "%s" => %o', (input, expected) => {
+  expect(parseTimestamps(input)).toEqual(expected)
 })
 
-test('parseTimestamps with VTT input and short formats', () => {
-  check('00:20.000 --> 00:24.400', {
-    start: 20000,
-    end: 24400
-  })
-
-  check('00:00.000 --> 00:01,000', {
-    start: 0,
-    end: 1000
-  })
-
-  check('59:50.050 --> 01:00:20.070', {
-    start: 3590050,
-    end: 3620070
-  })
-
-  check('0:59:50.050 --> 1:00:20.070', {
-    start: 3590050,
-    end: 3620070
-  })
+test.each([
+  [
+    '00:20.000 --> 00:24.400',
+    {
+      start: 20000,
+      end: 24400
+    }
+  ],
+  [
+    '00:00.000 --> 00:01,000',
+    {
+      start: 0,
+      end: 1000
+    }
+  ],
+  [
+    '59:50.050 --> 01:00:20.070',
+    {
+      start: 3590050,
+      end: 3620070
+    }
+  ],
+  [
+    '0:59:50.050 --> 1:00:20.070',
+    {
+      start: 3590050,
+      end: 3620070
+    }
+  ]
+])('parseTimestamps with VTT input and short format: "%s" => %o', (input, expected) => {
+  expect(parseTimestamps(input)).toEqual(expected)
 })
 
-test('parseTimestamps with VTT settings', () => {
-  check('12:34:56,789 --> 98:76:54,321 align:middle line:90%', {
-    start: 45296789,
-    end: 357414321,
-    settings: 'align:middle line:90%'
-  })
-
-  check(
+test.each([
+  [
+    '12:34:56,789 --> 98:76:54,321 align:middle line:90%',
+    {
+      start: 45296789,
+      end: 357414321,
+      settings: 'align:middle line:90%'
+    }
+  ],
+  [
     '00:20.000 --> 00:24.400 position:45%,line-right align:center size:35%',
     {
       start: 20000,
       end: 24400,
       settings: 'position:45%,line-right align:center size:35%'
     }
-  )
-
-  check(
+  ],
+  [
     '0:00:20.000 --> 0:00:24.400 position:45%,line-right align:center size:35%',
     {
       start: 20000,
       end: 24400,
       settings: 'position:45%,line-right align:center size:35%'
     }
-  )
+  ]
+])('parseTimestamps with VTT settings: "%s" => %o"', (input, expected) => {
+  expect(parseTimestamps(input)).toEqual(expected)
 })
--- test/toVttTime.test.js
+++ test/toVttTime.test.js
@@ -1,24 +1,11 @@
 import { toVttTime } from '../lib/toVttTime'
 
-test('should convert time from milliseconds to WebVTT format', () => {
-  const time1 = {
-    srt: '00:02:22.542',
-    ms: 120000 + 22000 + 542
-  }
-
-  const time2 = {
-    srt: '01:51:58.219',
-    ms: 3600000 + 3060000 + 58000 + 219
-  }
-
-  const time3 = {
-    srt: '00:00:00.000',
-    ms: 0
-  }
-
-  expect(toVttTime(time1.ms)).toEqual(time1.srt)
-  expect(toVttTime(time2.ms)).toEqual(time2.srt)
-  expect(toVttTime(time3.ms)).toEqual(time3.srt)
+test.each([
+  [ 120000 + 22000 + 542, '00:02:22.542' ],
+  [ 3600000 + 3060000 + 58000 + 219, '01:51:58.219' ],
+  [ 0, '00:00:00.000' ]
+])('toVttTime(%d) === %s', (ms, vtt) => {
+  expect(toVttTime(ms)).toBe(vtt)
 })
 
 test('should return the given values that are not numbers', () => {

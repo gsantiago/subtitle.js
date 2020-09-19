0a7932ce589abc1595be3316745b4391475b8dd6
Guilherme Santiago <guilherme.santiago95@gmail.com>
2020-09-19 12:48:52 -0300

    increase code coverage for parse

--- test/parseSync.test.ts
+++ test/parseSync.test.ts
@@ -356,3 +356,50 @@ Invalid timestamp
     new Error('expected timestamp at row 2, but received: "999Foo"')
   )
 })
+
+test('last captions with more than one line', () => {
+  const srt = `
+02:12:34,647 --> 02:12:35,489
+Hi.
+
+2
+02:12:36,415 --> 02:12:37,758
+Lois Lane.
+
+02:12:38,584 --> 02:12:40,120
+Welcome to the Planet.
+To the Earth Planet.
+The most beautiful planet in the whole universe.
+`
+
+  expect(parseSync(srt)).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "data": Object {
+          "end": 7955489,
+          "start": 7954647,
+          "text": "Hi.",
+        },
+        "type": "cue",
+      },
+      Object {
+        "data": Object {
+          "end": 7957758,
+          "start": 7956415,
+          "text": "Lois Lane.",
+        },
+        "type": "cue",
+      },
+      Object {
+        "data": Object {
+          "end": 7960120,
+          "start": 7958584,
+          "text": "Welcome to the Planet.
+    To the Earth Planet.
+    The most beautiful planet in the whole universe.",
+        },
+        "type": "cue",
+      },
+    ]
+  `)
+})

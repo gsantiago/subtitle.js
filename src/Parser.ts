1c06080483df18ca70c7989d0d35daec6cc36c07
Marcus Stenbeck <marcus.stenbeck@gmail.com>
2021-03-16 20:31:07 +0100

    ignore vtt format comments

--- src/Parser.ts
+++ src/Parser.ts
@@ -4,7 +4,7 @@ import { Node, RE_TIMESTAMP, parseTimestamps } from '.'
 export type Pusher = (node: Node) => void
 
 export interface ParseState {
-  expect: 'header' | 'id' | 'timestamp' | 'text'
+  expect: 'header' | 'id' | 'timestamp' | 'text' | 'vtt_comment'
   row: number
   hasContentStarted: boolean
   isWebVTT: boolean
@@ -36,6 +36,10 @@ export class Parser {
     return RE_TIMESTAMP.test(line)
   }
 
+  private isVttComment(line: string): boolean {
+    return /^NOTE/.test(line)
+  }
+
   private getError(expected: string, index: number, row: string): Error {
     return new Error(
       `expected ${expected} at row ${index + 1}, but received: "${row}"`
@@ -57,7 +61,8 @@ export class Parser {
       header: this.parseHeader,
       id: this.parseId,
       timestamp: this.parseTimestamp,
-      text: this.parseText
+      text: this.parseText,
+      vtt_comment: this.parseVttComment
     }[this.state.expect]
 
     parse.call(this, contents)
@@ -98,8 +103,21 @@ export class Parser {
       this.pushNode()
     }
 
-    if (!this.isIndex(line)) {
-      this.parseTimestamp(line)
+    if (this.isIndex(line)) return
+
+    if (this.state.isWebVTT && this.isVttComment(line)) {
+      this.state.expect = 'vtt_comment'
+      return
+    }
+
+    this.parseTimestamp(line)
+  }
+
+  private parseVttComment(line: string) {
+    this.state.expect = 'vtt_comment'
+
+    if (line.trim() === '') {
+      this.state.expect = 'id'
     }
   }
 
@@ -120,7 +138,12 @@ export class Parser {
   }
 
   private parseText(line: string) {
-    if (this.state.buffer.length > 0 && this.isTimestamp(line)) {
+    if (this.state.buffer.length === 0) {
+      this.state.buffer.push(line)
+      return
+    }
+
+    if (this.isTimestamp(line)) {
       const lastIndex = this.state.buffer.length - 1
 
       if (this.isIndex(this.state.buffer[lastIndex])) {
@@ -129,9 +152,16 @@ export class Parser {
 
       this.pushNode()
       this.parseTimestamp(line)
-    } else {
-      this.state.buffer.push(line)
+      return
     }
+
+    if (this.isVttComment(line)) {
+      this.pushNode()
+      this.parseVttComment(line)
+      return
+    }
+
+    this.state.buffer.push(line)
   }
 
   private pushNode(): void {

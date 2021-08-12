c919d14b3ff8daea8644a105e290536d9da7f24e
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2021-08-12 12:05:09 +0200

    Remove decimals from ms
    It can happen that ms is a value like
    200,00000003
    
    This removes the decimal halways having decimal as 3 digit

--- src/formatTimestamp.ts
+++ src/formatTimestamp.ts
@@ -10,7 +10,9 @@ export function formatTimestamp(
   const hours = date.getHours()
   const minutes = date.getMinutes()
   const seconds = date.getSeconds()
-  const ms = timestamp - (hours * 3600000 + minutes * 60000 + seconds * 1000)
+  const ms = Math.floor(
+    timestamp - (hours * 3600000 + minutes * 60000 + seconds * 1000)
+  )
 
   return `${padLeft(hours)}:${padLeft(minutes)}:${padLeft(seconds)}${
     options.format === 'WebVTT' ? '.' : ','
